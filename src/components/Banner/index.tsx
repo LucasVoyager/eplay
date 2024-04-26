2b4f5a8e9f59c20057313fafb8d7d331c6183f58
Lucas barros <fallenagamesytb@gmail.com>
2024-04-26 14:07:20 -0300

    feat: build 2 components the tag and button

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,28 @@
 import { Image, Prices, Title } from './styles'
 
 import bannerimg from '../../assets/images/Spiderman.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => {
   return (
     <Image style={{ backgroundImage: `url(${bannerimg})` }}>
       <div className="container">
-        <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
-        <Prices>
-          De <span>R$ 250,00</span> <br />
-          Por apenas R$ 99,90
-        </Prices>
+        <Tag size="big">Destaque do dia</Tag>
+        <div>
+          <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
+          <Prices>
+            De <span>R$ 250,00</span> <br />
+            Por apenas R$ 99,90
+          </Prices>
+        </div>
+        <Button
+          type="link"
+          to="/produto"
+          title="Clique aqui para aproveitar esta oferta"
+        >
+          Aproveitar
+        </Button>
       </div>
     </Image>
   )
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ButtonContainer, ButtonLink } from './styles'
+
+export type Props = {
+  type: 'button' | 'link'
+  title: string
+  to?: string
+  onClick?: () => void
+  children: string
+}
+
+const Button = ({ type, children, title, onClick, to }: Props) => {
+  if (type === 'button') {
+    return (
+      <ButtonContainer type="button" title={title} onClick={onClick}>
+        {children}
+      </ButtonContainer>
+    )
+  }
+
+  return <ButtonLink to={to as string}>{children}</ButtonLink>
+}
+
+export default Button
